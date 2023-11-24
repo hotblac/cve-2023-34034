f762f36db5364db843dc5d547bb17fab4bbdb4b5
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2023-11-24 16:46:15 +0000

    New project from start.spring.io

---
+++ src/main/java/org/dontpanic/cve202334034/Cve202334034Application.java
@@ -0,0 +1,13 @@
+package org.dontpanic.cve202334034;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cve202334034Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cve202334034Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/dontpanic/cve202334034/Cve202334034ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dontpanic.cve202334034;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cve202334034ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
